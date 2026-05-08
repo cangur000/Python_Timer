9185faeaecf607c1698e1964201d5e54cafc2a63
Cangur <112434361+cangur000@users.noreply.github.com>
2026-05-08 11:15:19 +0200

    Basic madlibs game to revise inputs and fprints

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+location = input("Enter the location: ")
+funthing = input("Enter the entertaining thing: ")
+surprise = input("Enter the random event: ")
+trueending = input("Enter the true location: ")
+
+print(f"Today I went to {location}.")
+print(f"I've had so much fun looking at the {funthing}")
+print(f"But then {surprise} happened")
+print(f"I was never at the {location}, it was all just a {trueending}")
